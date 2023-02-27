533b1719b25e22c77028eddde242a6bd56ce48db
Omkar <jadhavpatil2999@gmail.com>
2023-02-27 18:43:19 +0530

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  //0x6AA7C8B98425a5904d46f480588C2A3c45e19CD8
